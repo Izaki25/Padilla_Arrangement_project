17a228345a9973ad70abc9b95f882dc5fe001e1e
Izaki25 <padillakenneth440@gmail.com>
2024-11-13 09:24:45 +0800

    Add files via upload

---
+++ Padilla_la_excer3.java
@@ -0,0 +1,31 @@
+
+package padilla_la_excer3;
+
+
+public class Padilla_la_excer3 {
+
+    
+    public static void main(String[] args) {
+        
+        
+        
+        
+        int iNumber = 3;
+        
+        if(iNumber>=0)
+        {
+            System.out.println("First String");
+        }
+        
+        if (iNumber==0){
+            System.out.println("Second String");
+        }
+        
+        else{
+            System.out.println("Third String");
+        }
+        
+        
+    }
+    
+}
